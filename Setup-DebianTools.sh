dc985d346975c54c20b3b72b68435171d0f0cd50
Shrek007 <Anthony.asdf@gmail.com>
2020-12-21 23:22:40 -0700

    added File

---
+++ Setup-DebianTools.sh
@@ -0,0 +1,15 @@
+# Colorize Bash shell prompt if it exists
+FILE=~/.bashrc
+COLOREXPRESSION="\[\e[31m\][\[\e[m\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[38;5;153m\]\h\[\e[m\] \[\e[38;5;214m\]\W\[\e[m\]\[\e[31m\]]\[\e[m\]\\$ "
+if test -f "$FILE"; then
+    $X = export PS1=$COLOREXPRESSION
+    $X > $FILE
+fi
+
+# List of tools to install put into 'install' variable
+programs="\
+ openvpn\
+ ipcalc\
+"
+# 
+apt-get install $programs -y
--- Setup-ShellTools.ps1
+++ Setup-ShellTools.ps1
@@ -2,3 +2,13 @@
 
 # Enable OpenSSH
 Get-WindowsCapability -Online | Where Name -like "OpenSSH.client*" | Add-WindowsCapability -Online
+
+# OpenSSH config file is typically stored at %userprofile%\.ssh\
+if(!(Test-Path -Path $env:USERPROFILE\.ssh)){
+    Write-Output "Path does not exist"
+}
+else {
+    mkdir $env:USERPROFILE\.ssh
+}
+
+
