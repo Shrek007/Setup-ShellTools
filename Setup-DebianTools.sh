71b50ab826af5fe1e840342c9f0c4089e60cda37
Shrek007 <Anthony.asdf@gmail.com>
2020-12-23 22:12:17 -0700

    added firefox to apt install

--- Setup-DebianTools.sh
+++ Setup-DebianTools.sh
@@ -1,3 +1,5 @@
+# Aliases
+
 # Colorize Bash shell prompt if it exists
 FILE=~/.bashrc
 COLOREXPRESSION="\[\e[31m\][\[\e[m\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[38;5;153m\]\h\[\e[m\] \[\e[38;5;214m\]\W\[\e[m\]\[\e[31m\]]\[\e[m\]\\$ "
@@ -13,6 +15,7 @@ programs="\
  cmatrix\
  hollywood\
  tty-clock\
+ firefox\
 "
 # 
 apt-get install $programs -y
