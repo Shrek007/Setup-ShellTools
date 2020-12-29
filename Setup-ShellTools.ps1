c1486199378c58885975f17b7d2f113fb3be19a3
Shrek007 <Anthony.asdf@gmail.com>
2020-12-28 22:04:57 -0700

    Updated Alias Section - next step to create a separate file to download and call instead

--- Setup-ShellTools.ps1
+++ Setup-ShellTools.ps1
@@ -1,14 +1,47 @@
 ### Powershell
 
+### Functions
+function Get-file_exists {
+    param (
+        $Path
+    )
+    
+    if(!(Test-Path -Path $Path)){
+        Write-Output "Path to does not exist for:"
+        Write-Output "$Path"
+        Write-Output "Returning False..."
+        return $false
+    }
+    else {
+        return $Path
+    }
+}
+
 #### Package Managers
 # Chocolatey 
 Set-ExecutionPolicy AllSigned
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+
 # Alias
+$multi_line_aliases = @'
 set-alias -name grep -value Select-String
 set-alias -Name read -value Out-String
 set-alias -Name nano -Value 'C:\Program Files\Git\usr\bin\nano.exe'
+'@
+
+### Setup Profile
+if ((Get-file_exists -Path $profile) -eq $true) {
+    Write-Output "Profile exists:"
+    Write-Output $profile
+
+    Set-Content -Value $multi_line_aliases -Path $profile
+}
+
+# Alias - SEE Alias section above
+#set-alias -name grep -value Select-String
+#set-alias -Name read -value Out-String
+#set-alias -Name nano -Value 'C:\Program Files\Git\usr\bin\nano.exe'
 
 ### Git ###
 choco install git
@@ -23,5 +56,5 @@ if(!(Test-Path -Path $env:USERPROFILE\.ssh)){       # OpenSSH config file is typ
 }
 else {
     mkdir $env:USERPROFILE\.ssh
-    New-Item -ItemType file config                  # create empty ssh 'config' file
+    New-Item -ItemType file -Name config                  # create empty ssh 'config' file
 }
