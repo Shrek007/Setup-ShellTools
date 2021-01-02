0d7f33b2288bb718ff5f8a1cd7604ae6818ec081
Shrek007 <Anthony.asdf@gmail.com>
2021-01-01 22:50:00 -0700

    Started quick reference for commands w/ categories

--- Tips_and_Tricks.ps1
+++ Tips_and_Tricks.ps1
@@ -9,3 +9,71 @@ Get-NetTCPConnection | Out-String -stream | Select-String 80
 
 # Kill a process by PID
 Stop-process -ID 1337 -Force
+
+
+###########
+# https://stackify.com/powershell-commands-every-developer-should-know/
+##########
+#   Search/Find Commands
+#   LEARN
+Get-Command
+
+#   How to use Commands
+#   LEARN
+Get-Help
+
+#   Get command aliases
+#   VIEW STATE / GET INFO
+Get-Alias
+
+#   Configure command aliases
+#   SETTINGS
+Set-Alias
+
+#   Configure Env. to USE tools
+#   PRE-REQUISITS
+Set-ExecutionPolicy
+
+#   Find script execution settings
+#   VIEW STATE / GET INFO
+Get-ExecutionPolicy
+
+#   View list of services
+#   VIEW STATE / GET INFO
+Get-Service
+
+#   Convert to HTML
+#   OUTPUT FORMAT
+ConvertTo-Html
+
+#   Convert to CSV
+#   OUTPUT FORMAT
+Export-Csv
+
+#   Filter objects
+#   SORT / FILTER
+Where-Object
+
+#   Filter properties
+#   SORT / FILTER
+Select-Object
+
+#   View event log(s)
+#   VIEW STATE / GET INFO
+Get-EventLog -Log "Application"
+
+#   View list of currently running processes
+#   VIEW STATE / GET INFO
+Get-Process
+
+#   Delete command history
+#   DELETE DATA
+Clear-History
+
+
+# ???
+Invoke-Item
+Get-Event
+Add-History     # use case?
+Set-AuthenticodeSignature
+#   Question:   Get-Process   VS.   Get-Service
