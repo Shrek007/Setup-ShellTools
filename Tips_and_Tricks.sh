a1c820a64123964c7558b5a0a24b4b5acc386955
Shrek007 <Anthony.asdf@gmail.com>
2021-01-03 19:22:08 -0700

    Created File

---
+++ Tips_and_Tricks.sh
@@ -0,0 +1,4 @@
+# A great resource for all things using 'curl'
+curl cheat.sh               # How to use cheat.sh
+curl cheat.sh/:list         # list of all topics!
+curl cheat.sh/latency       # just cool
