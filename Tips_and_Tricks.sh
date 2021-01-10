fd5ec6ffcc6230e3ecc49c43854fb20aedb35320
Shrek007 <Anthony.asdf@gmail.com>
2021-01-09 22:59:09 -0700

    added examples for SED to swap & replace char

--- Tips_and_Tricks.sh
+++ Tips_and_Tricks.sh
@@ -3,6 +3,23 @@ curl cheat.sh               # How to use cheat.sh
 curl cheat.sh/:list         # list of all topics!
 curl cheat.sh/latency       # just cool
 
+# Quick & loud port scanner in bash with 3 second timeout
+curl 192.168.1.1:[3000-6000] -m 3 
+
+# Run multiple arguments through a single command
+for x in '*.gz'; do gunzip $x; done
+
+# Swap and replace characters
+sed 's/someRegex/replaceWithThis/numberOfFieldsInLineToReplace'
+
+sed 's/pattern/replace/'                            # Default is to only replace the 1st matching instance of a line
+sed 's!/home/user1!/home/user2!g'         # Sed can use any char for the separator, such as ':', which is useful when you don't want to have to use escape characters
+sed 's/pattern/replace/2'                           # Take from pipe, replace 2 pattern matches per line, output to stdout
+sed 's/pattern/replace/g'                           # Take from pipe, replace all pattern matches, output to stdout
+sed 's/pattern/replace/g' file.txt > output.txt     # Take from file.txt, dump to output.txt
+sed 's/pattern/replace/g' file.txt                  # Take from file.txt, dump to stdout
+sed -i 's/pattern/replace/g' output.txt             # Take from output.txt, -i for change to file inline, dump to output.txt
+
 ### Make API requests in Bash
 # create credential
 account_sid="ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
